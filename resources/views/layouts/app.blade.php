22c38689d0afd32a51aa20aa716b8ccbdb2f0e59
isikjon <isikjandev@gmail.com>
2025-11-09 12:37:12 +0500

    101% 09.11.2025

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="@yield('meta_description', 'Каталог анкет с подробными фильтрами и проверенными предложениями в Москве и Санкт-Петербурге.')">
     @php
         $styleCssVersion = file_exists(public_path('css/style.css')) ? filemtime(public_path('css/style.css')) : time();
