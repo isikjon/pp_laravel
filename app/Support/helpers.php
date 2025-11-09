6636cba8a70f9d617d327fcd6c9b7dea36cf2169
isikjon <isikjandev@gmail.com>
2025-11-09 12:55:40 +0500

    101% 09.11.2025

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -5,19 +5,8 @@ use Illuminate\Support\Facades\File;
 if (! function_exists('cached_asset')) {
     function cached_asset(string $path): string
     {
-        $version = File::exists(public_path($path)) ? File::lastModified(public_path($path)) : time();
-
-        if (str_starts_with($path, 'css/')) {
-            return route('assets.css', ['path' => substr($path, 4)]) . '?v=' . $version;
-        }
-
-        if (str_starts_with($path, 'js/')) {
-            return route('assets.js', ['path' => substr($path, 3)]) . '?v=' . $version;
-        }
-
-        if (str_starts_with($path, 'img/')) {
-            return route('assets.img', ['path' => substr($path, 4)]) . '?v=' . $version;
-        }
+        $fullPath = public_path($path);
+        $version = File::exists($fullPath) ? File::lastModified($fullPath) : time();
 
         return asset($path) . '?v=' . $version;
     }
