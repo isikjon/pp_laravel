b9f41be4ec2a14c80b29ab8fe85b0acf590c80be
isikjon <isikjandev@gmail.com>
2025-11-09 12:58:31 +0500

    101% 09.11.2025

--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,6 @@ use App\Http\Controllers\ContactRequestController;
 use App\Http\Controllers\MetroController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\FilterOptionsController;
-use App\Http\Controllers\AssetController;
-
-Route::get('/assets/css/{path}', [AssetController::class, 'css'])
-    ->where('path', '.*')
-    ->name('assets.css');
-
-Route::get('/assets/js/{path}', [AssetController::class, 'js'])
-    ->where('path', '.*')
-    ->name('assets.js');
-
-Route::get('/assets/img/{path}', [AssetController::class, 'image'])
-    ->where('path', '.*')
-    ->name('assets.img');
 
 Route::post('/contact-request', [ContactRequestController::class, 'store'])->name('contact.store');
 Route::get('/api/metro/list', [MetroController::class, 'getMetroList'])->name('metro.list');
