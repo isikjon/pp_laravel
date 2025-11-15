6cd34229a4f88f9e0e863c21e6fefdc4ea23056b
isikjon <isikjandev@gmail.com>
2025-11-16 00:51:52 +0500

    101% 16.11.2025

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->domain('prostitutkitest.com')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
